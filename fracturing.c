66b00d311646393bb95a2bf3e65a1f14f0aa6fb5
nathanf80 <55816700+nathanf80@users.noreply.github.com>
2024-09-08 07:13:11 +0000

    added perimeter funct

--- fracturing.c
+++ fracturing.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<math.h>
+#define pi 3.14159
 
 /*
 double getMultiple(int arg1) {
@@ -23,27 +24,52 @@ double askForUserInput() {
 }
 */
 
-double calculateDistance() {
-    //ask for user input of x1 and x2 values
-    printf("Enter x1 and x2:\n");
-    double x1, x2;
-    scanf("%lf %lf", &x1, &x2);
-
-    //ask for user input of y1 and y2 values
-    printf("Enter y1 and y2:\n");
-    double y1, y2;
-    scanf("%lf %lf", &y1, &y2);
+void printPoints(double x1, double x2, double y1, double y2){
 
     //print the first and second points
     printf("Point #1 entered: x1 = %lf; y1 = %lf\n", x1, y1);
     printf("Point #2 entered: x2 = %lf; y2 = %lf\n", x2, y2);
+}
 
+double findDistance(double x1, double x2, double y1, double y2){
+    //calculates the distance between points
     double distance = sqrt(pow(x2-x1, 2)+pow(y2-y1, 2));
-    printf("the distance between the 2 points is %lf\n",distance);
     return distance;
 }
 
+double calculateDistance() {
+    //ask user to input all x and y coords for both points used in the function
+    printf("Enter x1, x2, y1, and y2:\n");
+    double x1, x2, y1, y2;
+    scanf("%lf %lf %lf %lf", &x1, &x2, &y1, &y2);
+    
+    //prints points entered by the user
+    printPoints(x1,x2,y1,y2);
+
+    double distance = findDistance(x1,x2,y1,y2);
+    printf("The distance between the 2 points is %lf\n",distance);
+    return distance;
+}
+
+double calculatePerimeter(){
+    //ask user to input all x and y coords for both points used in the function
+    printf("Enter x1, x2, y1, and y2:\n");
+    double x1, x2, y1, y2;
+    scanf("%lf %lf %lf %lf", &x1, &x2, &y1, &y2);
+    
+    //prints points entered by the user
+    printPoints(x1,x2,y1,y2);
+
+    //calculates perimeter (circumference) of a circle with a diameter equal to the distance between entered points
+    double diameter = findDistance(x1,x2,y1,y2);
+    double circumference = pi*diameter;
+    printf("The perimeter of the city encompassed by your request is %lf\n", circumference);
+    return 2.5;
+
+}
+
 int main(int argc, char **argv) {
-    calculateDistance();
+    //calculateDistance();
+    calculatePerimeter();
     return 0;
 }
